edcf5347faa2e825628f7b63a64f9482c44357c0
John Doe <99072495@mydavinci.nl>
2022-09-07 14:00:55 +0200

    Eerste commit!

---
+++ module-01/demo.py
@@ -0,0 +1,14 @@
+
+lengte = input ('lengte:')
+lengte = float (lengte)
+
+breedte = input('breedte: ')
+breedte = float (breedte)
+
+
+opp = lengte*breedte
+om = 2*lengte+2*breedte
+
+
+print('oppervlakte wordt: ', opp)
+print('omtrek wordt: ', om)
---
+++ module-01/tutorial.py
@@ -0,0 +1,17 @@
+from xmlrpc.client import boolean
+
+
+print('esma')
+print('*' * 10)
+print('5' * 5)
+
+price = 5 * 6 -2
+print(price)
+print(5 * 6 -2)
+
+age = input('what is your name?: ')
+print('hello ' + age)
+
+name = input('what is your name: ')
+color = input('what is your favorite color? ')
+print(name + ' likes ' + color)
