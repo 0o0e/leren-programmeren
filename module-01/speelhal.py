eda44096510de352cd37b4d33e17b440b2a3d12c
John Doe <99072495@mydavinci.nl>
2022-09-08 11:11:39 +0200

    Eerste commit!

---
+++ module-01/speelhal.py
@@ -0,0 +1,11 @@
+personen = input('Met hoeveel personen ben je in totaal? ')
+personen = float (personen)
+prijs_tickets = 7.45 * personen
+
+vip_vr = input ('Hoeveel minuten ga je in totaal in de vip vr gameset? ')
+vip_vr = float (vip_vr)
+
+prijs_vip = 0.074 * vip_vr 
+
+totaalprijs = prijs_tickets + prijs_vip
+print(totaalprijs)
