8789fafd844c17a04db5c3926212fce86ecafcc3
John Doe <99072495@mydavinci.nl>
2022-09-10 00:28:38 +0200

    kaas_opdracht

---
+++ module-02/kaasflow.py
@@ -0,0 +1,35 @@
+print("beantwoord de vragen alleen met 'ja' of 'nee' ")
+
+kleur = input ('is de kaas geel? ')
+
+if kleur == 'ja':
+    gaten = input('zitten er gaten in? ')
+    if gaten == 'ja':
+        duur = input('is de kaas belagelijk duur?')
+        if duur == 'ja':
+            print('emmenthaler')
+        else:
+            print('leerdammer')
+
+    if gaten == 'nee':
+        hard = input('is de kaas hard als een steen?')
+        if hard == 'ja':
+            print('parmigiano reggiano')
+        else:
+            print('goudse kaas')
+
+if kleur == 'nee':
+    schimmel = input('heeft de kaas een blauwe schimmel? ')
+    if schimmel == 'ja':
+        korst = input('heeft de kaas een korst? ')
+        if korst == 'ja':
+            print('blue rochbaron')
+        else:
+            print("foume d'ambert")
+    if schimmel == 'nee':
+        korst_2 = input('heeft de kaas een korst? ')
+        if korst_2 == 'ja':
+            print('camembert')
+        else:
+         print('mozzarella')
+        
