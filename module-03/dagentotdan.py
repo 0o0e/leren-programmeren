1f7b29714f51957ce3ab7543d37d6580cd4afd90
John Doe <99072495@mydavinci.nl>
2022-10-05 13:03:27 +0200

    'dagentotdan'

---
+++ module-03/dagentotdan.py
@@ -0,0 +1,8 @@
+dag = input('welke dag van de week is het?(ma,di,wo,do,vr,za of zo) ')
+dagen = ['ma','di','wo','do','vr','za','zo']
+
+
+aaa = dagen[dagen.index(dag)+1:]
+while len(aaa) > 0:
+    print(aaa[0])
+    aaa = aaa [1:]
--- module-03/uren2.py
+++ module-03/uren2.py
@@ -1,7 +1,9 @@
 i = 0
-for i in range(2):
-    while i < 13:
-        print(i, 'A.M')
-        i = i + 1
-        print(i, 'P.M')
-        i = i +1
+while i < 13:
+    print(i, 'A.M')
+    i = i + 1
+
+i = 0
+while i < 13:
+    print(i, 'P.M')
+    i = i + 1
