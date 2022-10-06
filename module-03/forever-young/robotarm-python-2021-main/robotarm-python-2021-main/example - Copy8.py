ca559a06e4238b3b8d36099cf1c0069066d16a67
John Doe <99072495@mydavinci.nl>
2022-10-06 13:17:29 +0200

    'copy8'

---
+++ module-03/forever-young/robotarm-python-2021-main/robotarm-python-2021-main/example - Copy8.py
@@ -0,0 +1,19 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 8')
+robotArm.speed = 4
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.moveRight()
+aaa = 0
+while aaa < 7:
+    for i in range(8):
+        robotArm.grab() , robotArm.moveRight()
+    robotArm.drop()
+    for x in range (8):
+        robotArm.moveLeft()
+    aaa = aaa + 1
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
