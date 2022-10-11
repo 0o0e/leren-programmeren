524c5d37aeb90ed62bc90691b916dbe62e972198
John Doe <99072495@mydavinci.nl>
2022-10-11 10:26:37 +0200

    'copy11'

--- module-03/forever-young/robotarm-python-2021-main/robotarm-python-2021-main/example - Copy11.py
+++ module-03/forever-young/robotarm-python-2021-main/robotarm-python-2021-main/example - Copy11.py
@@ -1,10 +1,7 @@
 from RobotArm import RobotArm
-
 robotArm = RobotArm('exercise 11')
 robotArm.speed = 3
-
-# Jouw python instructies zet je vanaf hier:
-for ii in range(9):
+for i in range(9):
     robotArm.grab()
     robotArm.scan()
     if robotArm.scan() == 'white':
@@ -14,12 +11,4 @@ for ii in range(9):
     else:
         robotArm.drop()
         robotArm.moveRight()
-
-
-
-
-
-
-
-# Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
