9d9cfb6f7a593b3b394881e5454fed338ecc3e46
John Doe <99072495@mydavinci.nl>
2022-10-13 09:38:34 +0200

    'xwembad_stap3'

--- challenges/zwembadkost.py
+++ challenges/zwembadkost.py
@@ -2,12 +2,17 @@
 m3 = round(float(8 * 3 * 1.5))
 print(m3)
 
-# stap 2
+# stap 2 en 3
 uitgravengrond = float(m3 * 25)
 afvoerengrond = float(m3 * 32.50)
-totaal = float (uitgravengrond + afvoerengrond)
+voorijkosten = round(2.05 * 10)
+voorrijkosten = voorijkosten + 250
+totaal = float (uitgravengrond + afvoerengrond + voorrijkosten)
+
 print(f""" 
 offerte voor een zwembad van 8 bij 3 bij 1.5 meter (inhoud : 36 m3
 uitgraven: ${uitgravengrond}
 afvoeren: ${afvoerengrond}
-totaal: ${totaal}""")
+voorrijkosten ${voorrijkosten}
+totaal: ${totaal}
+""")
