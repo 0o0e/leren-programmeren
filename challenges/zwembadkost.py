cf1ca99c751e92b98d1eaa04afb8b3070ebdf75a
John Doe <99072495@mydavinci.nl>
2022-10-13 10:17:49 +0200

    'zwembad_4update'

--- challenges/zwembadkost.py
+++ challenges/zwembadkost.py
@@ -12,24 +12,25 @@ afvoerengrond = float(m3 * 32.50)
 
 afstand = float(input('Wat is de afstand vanaf het bedrijf naar u? '))
 if afstand < 50 and m3 < 20:
-    voorrijkosten = 100.0
+    extra = afstand * 1.25
+    voorrijkosten = 100.00 + extra
 if afstand < 50 and m3 > 20:
-    voorrijkosten = 250.0
+    extra = afstand * 2.15
+    voorrijkosten = 250.0 + extra
 if afstand > 50 and m3 < 20:
-    voorrijkosten = 100.0
-    diff = float(afstand - 50)
-    extra = diff * 1.15
+    extra = afstand * 1.15
+    voorrijkosten = 100.0 + extra
 if afstand > 50 and m3 > 20:
-    voorrijkosten = 250.0
-    diff = float(afstand - 50)
-    extra = diff * 2.05
+    extra = afstand * 2.05
+    voorrijkosten = 250.0 + extra
+
 
 totaal = float (uitgravengrond + afvoerengrond + voorrijkosten)
 
 # voorrijkosten kloppen alleen als de klant 60 km verweg woont en als de grootte meer dan 20 m3 is dus ik heb er een input van gemaakt
 
 print(f"""
-offerte voor een zwembad van 8 bij 3 bij 1.5 meter (inhoud : 36 m3
+offerte voor een zwembad van 8 bij 3 bij 1.5 meter (inhoud : {m3} m3)
 uitgraven: ${uitgravengrond}
 afvoeren: ${afvoerengrond}
 voorrijkosten ${voorrijkosten}
