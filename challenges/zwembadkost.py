a7a51119451357d309a7701bb122f33364beef99
John Doe <99072495@mydavinci.nl>
2022-10-13 09:33:03 +0200

    'zwembad_stap2'

--- challenges/zwembadkost.py
+++ challenges/zwembadkost.py
@@ -1,3 +1,13 @@
 # stap 1
-m3 = float(8 * 3 * 1.5)
-print(round(m3))
+m3 = round(float(8 * 3 * 1.5))
+print(m3)
+
+# stap 2
+uitgravengrond = float(m3 * 25)
+afvoerengrond = float(m3 * 32.50)
+totaal = float (uitgravengrond + afvoerengrond)
+print(f""" 
+offerte voor een zwembad van 8 bij 3 bij 1.5 meter (inhoud : 36 m3
+uitgraven: ${uitgravengrond}
+afvoeren: ${afvoerengrond}
+totaal: ${totaal}""")
