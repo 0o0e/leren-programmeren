e73e692806d7ad2489f7219e589ba2b63096b623
John Doe <99072495@mydavinci.nl>
2022-11-15 12:55:24 +0100

    deck_spelkaaryten

---
+++ module-04/deck_van_speelkaarten.py
@@ -0,0 +1,19 @@
+import random
+colors = ['harten','klaveren','schoppen','ruiten']
+values = [2,3,4,5,6,7,8,9,10,'boer','vrouw','heer','aas']
+jokers = ['joker']
+deck = []
+for color in colors:
+    for value in values:
+        deck.append((color,value,))
+for i in range(2):
+    deck.append((jokers))
+teller = 1
+random.shuffle(deck)
+for i in range(7):
+    choice =random.choice(deck)
+    print(f'kaart {teller} {choice}')
+    deck.remove(choice)
+    teller += 1
+print(f'deck {len(deck)} kaarten: , {deck}')
+
