8c295fa06876794a00be2ca50d961300a42ff596
John Doe <99072495@mydavinci.nl>
2023-01-10 10:52:41 +0100

    lootjes_update

--- module-04/lootjes.py
+++ module-04/lootjes.py
@@ -1,25 +1,26 @@
 import random
 
-waar = True
+waarofnietwaar = True
 
 namen = []
 naam = {}
 nieuwelijst = []
-teller = 3
-while waar == True:
-    name = input(f'noem een naam : ')
+
+
+aantal= int(input('Hoeveel deelnemers zijn er? '))
+while aantal < 3:
+    print('minimaal 3 namen')
+    aantal= int(input('Hoeveel deelnemers zijn er? '))
+
+teller = 1
+while teller <= aantal:
+    name = input(f'wat is de naam van deelnemer {teller}: ')
     if name.lower() in namen:
-        print('alleen unieke namen')
+        print('alleen unieke namen ')
     else:
         namen.append(name)
         nieuwelijst.append(name)
-        teller -= 1
-    if len(namen) < 3:
-        print(f'nog minimaal {teller} namen')
-    if len(namen) >= 3:
-        meer = input('wil je meer namen toevoegen? (ja/nee) ').lower()
-        if meer != 'ja':
-            waar = False
+        teller +=1
 
 tellen = 0
 for i in (namen):
@@ -33,14 +34,12 @@ for i in (namen):
             random_naam = random.choice(nieuwelijst)
             while i == random_naam:
                 random_naam = random.choice(nieuwelijst)
-            else:
-                naam[i] = random_naam
-                nieuwelijst.remove(random_naam)
+            naam[i] = random_naam
+            nieuwelijst.remove(random_naam)
     else:
         while i == random_naam:
             random_naam = random.choice(nieuwelijst)
         else:
             naam[i] = random_naam
             nieuwelijst.remove(random_naam)
-
 print(naam)
