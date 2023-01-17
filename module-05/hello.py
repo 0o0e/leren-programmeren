dcb9bd51979b9b9d617baff8efbd2141ab5af7dd
John Doe <99072495@mydavinci.nl>
2023-01-17 11:23:01 +0100

    update

--- module-05/hello.py
+++ module-05/hello.py
@@ -1,4 +1,7 @@
-def hello_py():
-    print('Hello from function town')
+def hello_py(a):
+    teller = 1
+    for i in range(0,a):
+        print(f'Hello from function town {teller} ')
+        teller += 1
 
-hello_py()
+hello_py(7)
