410b408f8f5d6ec265f237440c7069e1c3232492
John Doe <99072495@mydavinci.nl>
2023-01-17 09:48:15 +0100

    update

---
+++ module-04/lootjes copy 2.py
@@ -0,0 +1,46 @@
+import random
+
+
+namen = []
+naam = {}
+nieuwelijst = []
+
+# aantal deelnemers
+aantal= int(input('Hoeveel deelnemers zijn er? '))
+while aantal < 3:
+    print('minimaal 3 namen')
+    aantal= int(input('Hoeveel deelnemers zijn er? '))
+
+teller = 1
+while len(namen) < aantal:
+    name = input(f'wat is de naam van deelnemer {len(namen)+1}: ')
+    if name.lower() in namen:
+        print('alleen unieke namen ')
+    else:
+        namen.append(name)
+        nieuwelijst.append(name)
+        teller +=1
+
+
+
+tellen = 0
+ok = False
+while not ok:
+    print(nieuwelijst)
+    for i in namen:
+        random_naam = random.choice(nieuwelijst)
+        if len(nieuwelijst) == 1 and random_naam == i:
+            nieuwelijst.clear()
+            naam.clear()
+            for namn in namen:
+                nieuwelijst.append(namn)
+            # ok = False
+        else:
+            random_naam = random.choice(nieuwelijst)
+            while random_naam == i:
+                random_naam = random.choice(nieuwelijst)
+            naam[i] = random_naam
+            nieuwelijst.remove(random_naam)
+    if len(nieuwelijst) == 0:
+        ok = True
+print(naam)
