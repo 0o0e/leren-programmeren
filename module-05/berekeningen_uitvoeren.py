a5f83cfb5260a27fa7b2aa74eed5c350d75de185
John Doe <99072495@mydavinci.nl>
2023-01-25 09:47:27 +0100

    update

---
+++ module-05/berekeningen_uitvoeren.py
@@ -0,0 +1,13 @@
+def addition(number1: int,number2:int):
+    return number1 + number2
+
+print(addition(2,3))
+
+def subtraction(number1: int,number2:int):
+    return number1 - number2
+
+def multiplication(number1: int,number2:int):
+    return number1 * number2
+
+def division(number1: int,number2:int):
+    return number1 / number2
--- module-05/namen_en_leeftijden.py
+++ module-05/namen_en_leeftijden.py
@@ -1,20 +1,15 @@
 
-def namen_en_leeftijd():
-    lijst = []
-    while True:
-        naam = input('noem een naam: ')
-        leeftijd = input('noem een leeftijd: ')
-        lijst.append({
-            'name' : naam,
-            'age' : leeftijd
-        })
-        stoppenofniet = input('Toets enter om door te gaan of stop om te printen ')
-        if stoppenofniet == 'stop':
-            break
-    for i in lijst:
-        name = i['name']
-        age = i['age']
-        print(f'{name} is {age} jaar')
 
+def namen_leeftijden(dict):
+    for i in dict:
+        print(i,'is',dict[i],'jaar oud')
 
-namen_en_leeftijd()
+gegevens={}
+stoppenofniet = 'ja'
+while stoppenofniet != 'stop':
+    naam = input('noem een naam: ')
+    leeftijd = input('noem een leeftijd: ')
+    gegevens.update({naam :leeftijd})
+    stoppenofniet = input('Toets enter om door te gaan of stop om te printen ')
+namen_leeftijden(gegevens)
+print(gegevens)
