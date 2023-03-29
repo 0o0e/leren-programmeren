1235c27e9159f224da8143351536b972cb716f36
John Doe <99072495@mydavinci.nl>
2023-03-29 13:37:04 +0200

    ijssalon_update

--- module-05/ijssalon.py
+++ module-05/ijssalon.py
@@ -15,14 +15,15 @@ def bakje_of_hoorntje(aantal_bollen):
     global hoorn_of_bak
     hoorn_of_bak = ''
     while hoorn_of_bak != 'hoorntje' or hoorn_of_bak != 'bakje':
-        print(hoorn_of_bak)
         if aantal_bollen <= 3:
             hoorn_of_bak = input('Wilt u een hoorntje of een bakje? ')
-        elif aantal_bollen >= 4 and aantal_bollen <= 8:
+        elif aantal_bollen >= 4 and aantal_bollen < 8:
             hoorn_of_bak = 'bakje'
-        elif aantal_bollen > 8:
+        elif aantal_bollen >= 8:
+            print('sorry, zulke grote bakken hebben we niet. ')
             return('sorry, zulke grote bakken hebben we niet. ')
         else:
             print('sorry, dat snap ik niet. ')
-    if hoorn_of_bak == 'hoorntje' or hoorn_of_bak == 'bakje':
-        return (f'hier is uw {hoorn_of_bak} met {aantal_bollen} bolletjes.')
+        if hoorn_of_bak == 'bakje' or hoorn_of_bak == 'hoorntje':
+            print(f'hier is uw {hoorn_of_bak} met {aantal_bollen} bolletjes.')
+            return
--- module-05/ijssalon_flow.py
+++ module-05/ijssalon_flow.py
@@ -1,26 +1,21 @@
 from ijssalon import *
-a = True
+bestellen = True
 
-while a == True:
-    # meer = ''
+while bestellen == True:
     meer_bestellen = True
 
     hoorn_bak = bakje_of_hoorntje(aantal_bolletjes())
-    print(hoorn_bak)
-    print('dd')
-    while hoorn_bak == ('sorry, zulke grote bakken hebben we niet. '):
-        hoorn_bak = bakje_of_hoorntje(aantal_bolletjes())
-        print(hoorn_bak)
+    if hoorn_bak == 'sorry, zulke grote bakken hebben we niet. ':
+        while hoorn_bak == ('sorry, zulke grote bakken hebben we niet.'):
+            hoorn_bak = bakje_of_hoorntje(aantal_bolletjes())
 
     while meer_bestellen == True:
         meer = input('wilt u meer bestellen? ')
-        if meer == 'nee' and meer != 'ja':
-            a = False
-            b = False
+        if meer == 'nee':
+            bestellen = False
             meer_bestellen = False
         elif meer != 'ja' and meer != 'nee':
             print('sorry, dat snap ik niet')
-            continue
         elif meer == 'ja':
             meer_bestellen = False
             
