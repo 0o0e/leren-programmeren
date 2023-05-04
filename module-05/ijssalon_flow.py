cfd2abddc62fc0e527ec7f15eeb7f4ac8b170d60
John Doe <99072495@mydavinci.nl>
2023-05-04 23:51:05 +0200

    updateijssalon

--- module-05/ijssalon_flow.py
+++ module-05/ijssalon_flow.py
@@ -4,15 +4,23 @@ bolletje_pr = 0
 hoorntje_pr = 0
 bakje_pr = 0
 
+
 aantal_bak = 0
 aantal_hoorn = 0
 
+
+smakenlijst = []
 while bestellen == True:
     meer_bestellen = True
     
     
 
     aantal_bollen = aantal_bolletjes()
+    smaak = bolletje_smaak(aantal_bollen)
+
+    smakenlijst += smaak
+
+
     hoorn_bak = kies_bakje_of_hoorntje(aantal_bollen)
     bolletje_pr += round((1.10 * aantal_bollen),2)
 
@@ -36,7 +44,7 @@ while bestellen == True:
     if verder == 'nee':
         bestellen = False
         meer_bestellen = False
-bon(aantal_bollen,bolletje_pr,hoorntje_pr,bakje_pr,aantal_bak,aantal_hoorn)
+bon(bolletje_pr,hoorntje_pr,bakje_pr,aantal_bak,aantal_hoorn,smakenlijst)
 
 
 # while meer_bestellen == True:
