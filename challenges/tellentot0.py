63e3f65848ad315425693317df6960478d261265
John Doe <99072495@mydavinci.nl>
2023-06-09 09:52:57 +0200

    update

--- challenges/tellentot0.py
+++
@@ -1,8 +0,0 @@
-welkenummer = int(input('Kies een nummer: '))
-
-while welkenummer != 0:
-    if welkenummer > 0:
-        welkenummer = welkenummer - 1
-    if welkenummer < 0:
-        welkenummer = welkenummer + 1
-    print(welkenummer)
--- challenges/zookeeper.py
+++
@@ -1,6 +0,0 @@
-def berekenen(giraffen : int,struisvogels : int,zebras: int):
-    giraffen *= 4
-    struisvogels *= 2
-    zebras *= 4
-    return giraffen + struisvogels + zebras
-    
